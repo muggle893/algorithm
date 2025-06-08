b48899c58be1abd148c3e86cd9a8c1192baf4a8a
txf893 <txf893@126.com>
2025-06-08 20:57:14 +0800

    leetcode93 复原ip地址题解

---
+++ practice2_backtrace/src/code_6_8/leecode93_复原IP地址.java
@@ -0,0 +1,65 @@
+package code_6_8;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class leecode93_复原IP地址 {
+    private List<String> res = new ArrayList<>();
+    private String path = "";
+    private int ipNumCount = 0; // 保存ip中的整数个数
+    public List<String> restoreIpAddresses(String s) {
+        backtrace(s, 0);
+        return res;
+    }
+
+    @Test
+    public void test() {
+        res = restoreIpAddresses("25525511135");
+        for (String s : res) {
+            System.out.println(s);
+        }
+    }
+
+    private boolean isLegalNum(String num) {
+        // 不能包含前导0
+        if (num.length() > 1 && num.charAt(0) == '0') {
+            return false;
+        }
+        // 判断范围
+        int val = Integer.valueOf(num);
+        if (val >= 0 && val <= 255) {
+            return true;
+        }
+        return false;
+    }
+    private void backtrace(String s, int startIndex) {
+        if (startIndex == s.length() && ipNumCount == 4) {
+            // 收集结果
+            res.add(new String(path));
+            return;
+        }
+        // 1-3位数字，注意前导0问题
+        for (int i = startIndex + 1; i <= startIndex + 3 && i <= s.length(); i++) {
+            // 4层了就不能继续往下了
+            if (ipNumCount == 4) {
+                break;
+            }
+            // 截取数字
+            String num = s.substring(startIndex, i);
+            if (!isLegalNum(num)) {
+                continue;
+            }
+            ipNumCount++;
+            int endIndex = path.length();
+            path += s.substring(startIndex, i);
+            if (ipNumCount < 4) {
+                path += ".";
+            }
+            backtrace(s, i);
+            ipNumCount--;
+            path = path.substring(0, endIndex);
+        }
+    }
+}
