0dc4ff6cd7df04d9057dd87973e6cb191e6c4201
txf893 <txf893@126.com>
2025-06-12 12:05:53 +0800

    子集II题解更新

--- practice2_backtrace/src/code_6_9/leetcode90_子集II.java
+++ practice2_backtrace/src/code_6_9/leetcode90_子集II.java
@@ -1,5 +1,7 @@
 package code_6_9;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,15 +9,31 @@ import java.util.List;
 public class leetcode90_子集II {
     private List<List<Integer>> res = new ArrayList<>();
     private List<Integer> path = new ArrayList<>();
+
+    @Test
+    public void test() {
+        int[] arr = {1,2,2};
+        List<List<Integer>> li = subsetsWithDup(arr);
+        for (List<Integer> subLi : li) {
+            for (Integer e : subLi) {
+                System.out.print(e);
+            }
+            System.out.println();
+        }
+
+    }
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         boolean[] used = new boolean[nums.length];
         Arrays.fill(used, false);
+        Arrays.sort(nums);
         backtrace(nums, 0, used);
-        res.add(0, new ArrayList<>());
         return res;
     }
 
     private void backtrace(int[] nums, int startIndex, boolean[] used) {
+        // 收集结果
+        res.add(new ArrayList<>(path));
         // 递归返回
         if (startIndex == nums.length) {
             return;
@@ -25,7 +43,6 @@ public class leetcode90_子集II {
                 continue;
             }
             path.add(nums[i]);
-            res.add(new ArrayList<>(path));
             used[i] = true;
             int lastIndex = path.size() - 1;
             backtrace(nums, i + 1, used);
